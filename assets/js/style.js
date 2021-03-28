efa22964d243ea1208dba7b513acc8e1c08f8941
NTD277 <ntd270799@gmail.com>
2021-03-28 23:44:52 +0700

    Hoan thanh trang desktop

---
+++ assets/js/style.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    $(".social ul li a").hover(function() {
+        $(this).find(".social-hover").css("display", "initial")
+    }, function() {
+        $(this).find(".social-hover").css("display", "none")
+    });
+    $(".items").hover(function() {
+            $(this).find(".information-featured").css("display", "initial");
+            $(this).find(".owl-nav").css("display", "initial");
+        },
+        function() {
+            $(".information-featured").css("display", "none");
+            $(".owl-nav").css("display", "none");
+        });
+    $("footer .container .nav a").hover(function() {
+            $(this).css("color", "#d6ba8d");
+        },
+        function() {
+            $(this).css("color", "#ababab");
+        }
+    );
+    $("footer .social a").hover(function() {
+            $(this).css("background-color", "#d6ba8d");
+            $(this).find("i").css("background-color", "#d6ba8d");
+            $(this).find("i").css("color", "#333333");
+        },
+        function() {
+            $(this).css("background-color", "#333333");
+            $(this).find("i").css("background-color", "#333333");
+            $(this).find("i").css("color", "#ffffff");
+        }
+    );
+
+});
+
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive: {
+            320: {
+                items: 1
+            }
+        }
+    })
+});
